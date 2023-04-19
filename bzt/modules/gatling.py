91005a00ae255bf7faf9813c2675366976691ae2
Pavel Marik <pmarik@perforce.com>
2023-04-19 15:31:59 +0200

    test-sel-server

--- bzt/modules/gatling.py
+++ bzt/modules/gatling.py
@@ -702,7 +702,7 @@ class Gatling(RequiredTool):
     """
     DOWNLOAD_LINK = "https://repo1.maven.org/maven2/io/gatling/highcharts/gatling-charts-highcharts-bundle" \
                     "/{version}/gatling-charts-highcharts-bundle-{version}-bundle.zip"
-    VERSION = "3.9.2"
+    VERSION = "3.7.6"
     LOCAL_PATH = "~/.bzt/gatling-taurus/{version}/bin/gatling{suffix}"
 
     def __init__(self, config=None, **kwargs):
